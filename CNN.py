93dfc778270787736a0f73a7b85b2cc3b0670d65
playerInEminor <51229501+playerInEminor@users.noreply.github.com>
2019-11-28 16:24:41 -0500

    CNN and KNN(tested ok)

---
+++ CNN.py
@@ -0,0 +1,32 @@
+from keras.models import Sequential
+from keras.layers import Conv2D, MaxPooling2D, Dense, Flatten
+from keras.losses import categorical_crossentropy
+from keras.optimizers import Adam
+from keras.utils import to_categorical
+
+def cnnModel(X_train, y_train, X_Test, y_test):
+    num_filters = 8
+    filter_size = 3
+    pool_size = 2
+
+    model = Sequential()
+
+    model.add(Conv2D(32, kernel_size=3, strides=1,
+                     activation='relu',
+                     input_shape=()))
+    model.add(MaxPooling2D(pool_size=(2, 2), strides=(2, 2)))
+    model.add(Conv2D(64, (5, 5), activation='relu'))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+    model.add(Flatten())
+    model.add(Dense(1000, activation='relu'))
+    model.add(Dense(3, activation='softmax'))
+
+    model.compile(
+        loss=categorical_crossentropy,
+        optimizer=Adam,
+        metrics=['accuracy']
+    )
+
+    model.fit(X_train, to_categorical(y_train), epochs=5)
+
+    model.evaluate(X_Test, to_categorical(y_test))
---
+++ KNN.py
@@ -0,0 +1,51 @@
+from numpy.random import normal
+from sklearn import neighbors
+import numpy as np
+
+def initData():
+    x1 = normal(50, 6, 200)
+    y1 = normal(5, 0.5, 200)
+
+    x2 = normal(30, 6, 200)
+    y2 = normal(4, 0.5, 200)
+
+    x3 = normal(45, 6, 200)
+    y3 = normal(2.5, 0.5, 200)
+    #initialize data
+
+    x_val = np.concatenate((x1, x2, x3))
+    y_val = np.concatenate((y1, y2, y3))
+    x_diff = max(x_val) - min(x_val)
+    y_diff = max(y_val) - min(y_val)
+    x_normalized = [x / (x_diff) for x in x_val]
+    y_normalized = [y / (y_diff) for y in y_val]
+    xy_train_normalized = list(zip(x_normalized, y_normalized))
+
+    labels_train = [1] * 200 + [2] * 200 + [3] * 200
+
+    x1_test = normal(50, 6, 100)
+    y1_test = normal(5, 0.5, 100)
+
+    x2_test = normal(30, 6, 100)
+    y2_test = normal(4, 0.5, 100)
+
+    x3_test = normal(45, 6, 100)
+    y3_test = normal(2.5, 0.5, 100)
+
+    xy_test_normalized = list(zip(np.concatenate((x1_test, x2_test, x3_test)) / x_diff, \
+                             np.concatenate((y1_test, y2_test, y3_test)) / y_diff))
+
+    labels_test = [1] * 100 + [2] * 100 + [3] * 100
+
+    return xy_train_normalized, labels_train, xy_test_normalized, labels_test
+
+def knnModel(X_train, y_train, X_Test, y_test):
+    model = neighbors.KNeighborsClassifier(n_neighbors=5, weights='uniform', algorithm='auto', leaf_size=1, p=2, metric='minkowski')
+    model.fit(X_train, y_train)
+    score = model.score(X_Test, y_test)
+    print(score)
+
+X_train, y_train, X_Test, y_test = initData()
+
+knnModel(X_train, y_train, X_Test, y_test)
+
